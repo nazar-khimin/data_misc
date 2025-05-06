f8ba1f4f38b7b6b9a31eb753ff771956268cf80b
nazar.khimin <nazar.khimin@unanet.com>
2025-05-06 19:32:31 +0300

    optimizations

--- airflow_practice/pii/dags/duck_db/pipeline_dag.py
+++ airflow_practice/pii/dags/duck_db/pipeline_dag.py
@@ -1,11 +1,9 @@
 import pendulum
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import dag
 
-from duck_db.run_data_generation_task import generate_raw_data_and_write_to_csv
-from duck_db.run_golden_dbt_task import run_dbt_golden_task
-from duck_db.run_silver_dbt_task import run_dbt_silver_task
-
-from airflow_practice.pii.dags.duck_db.tasks.run_bronze_dbt_task import run_dbt_bronze_task
+from run_data_generation_task import generate_raw_data_and_write_to_csv
 
 default_args = {
     'owner': 'airflow',
@@ -24,11 +22,32 @@ default_args = {
 )
 def main_dag():
     data_csv = "/opt/airflow/data/raw_data.csv"
+
+    generate_raw_data_task = PythonOperator(
+        task_id="generate_raw_data",
+        python_callable=generate_raw_data_and_write_to_csv,
+        op_args=[data_csv],
+    )
+    bronze_models = BashOperator(
+        task_id='run_dbt_bronze',
+        bash_command='set -x; cd /opt/airflow/dbt && dbt run --select tag:bronze'
+    )
+
+    silver_models = BashOperator(
+        task_id='run_dbt_silver',
+        bash_command='set -x; cd /opt/airflow/dbt && dbt run --select tag:silver'
+    )
+
+    golden_models = BashOperator(
+        task_id='golden',
+        bash_command='set -x; cd /opt/airflow/dbt && dbt run --select tag:golden'
+    )
+
     (
-            generate_raw_data_and_write_to_csv(data_csv) >>
-            run_dbt_bronze_task() >>
-            run_dbt_silver_task() >>
-            run_dbt_golden_task()
+            generate_raw_data_task >>
+            bronze_models >>
+            silver_models >>
+            golden_models
     )
 
 main_dag()
