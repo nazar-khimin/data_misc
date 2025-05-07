1fc54281a1b78e7188c492dbc0224fc0df8d572a
nazar.khimin <nazar.khimin@unanet.com>
2025-05-07 15:07:17 +0300

    added dependecy

--- airflow/pii/dags/duck_db/transformations.py
+++ airflow/pii/dags/duck_db/transformations.py
@@ -1,10 +1,20 @@
 import subprocess
 from datetime import datetime
 
+from airflow.providers.standard.sensors.external_task import ExternalTaskSensor
 from airflow.sdk import dag, task
 
 @dag(schedule=None, start_date=datetime(2023, 1, 1), catchup=False)
-def dbt_dag():
+def duck_dbt_dag():
+    wait_for_data_gen = ExternalTaskSensor(
+        task_id="wait_for_data_gen",
+        external_dag_id="raw_generation_dag",
+        external_task_id="generate_data_task",
+        timeout=30,
+        poke_interval=5,
+        mode="poke",
+    )
+
     @task
     def run_dbt(command: str):
         subprocess.run(command, shell=True, check=True)
@@ -19,6 +29,6 @@ def dbt_dag():
     silver = run_dbt.override(task_id="run_silver")(dbt_commands["silver"])
     golden = run_dbt.override(task_id="run_golden")(dbt_commands["golden"])
 
-    bronze >> silver >> golden
+    wait_for_data_gen >> bronze >> silver >> golden
 
-dag_instance = dbt_dag()
+dag_instance = duck_dbt_dag()
