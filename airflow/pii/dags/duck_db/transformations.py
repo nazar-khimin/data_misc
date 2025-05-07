4e72aa00c25867b5b4c6b3300d9daddc2265389c
nazar.khimin <nazar.khimin@unanet.com>
2025-05-07 13:49:57 +0300

    fixed transformations dag

--- airflow/pii/dags/duck_db/transformations.py
+++ airflow/pii/dags/duck_db/transformations.py
@@ -5,14 +5,20 @@ from airflow.sdk import dag, task
 
 @dag(schedule=None, start_date=datetime(2023, 1, 1), catchup=False)
 def dbt_dag():
-    @task()
+    @task
     def run_dbt(command: str):
         subprocess.run(command, shell=True, check=True)
 
-    bronze_models = run_dbt('set -x; cd /opt/airflow/dbt && dbt run --select tag:bronze')
-    silver_models = run_dbt('set -x; cd /opt/airflow/dbt && dbt run --select tag:silver')
-    golden_models = run_dbt('set -x; cd /opt/airflow/dbt && dbt run --select tag:golden')
+    dbt_commands = {
+        "bronze": "set -x; cd /opt/airflow/dbt && dbt run --select tag:bronze",
+        "silver": "set -x; cd /opt/airflow/dbt && dbt run --select tag:silver",
+        "golden": "set -x; cd /opt/airflow/dbt && dbt run --select tag:golden",
+    }
 
-    bronze_models >> silver_models >> golden_models
+    bronze = run_dbt.override(task_id="run_bronze")(dbt_commands["bronze"])
+    silver = run_dbt.override(task_id="run_silver")(dbt_commands["silver"])
+    golden = run_dbt.override(task_id="run_golden")(dbt_commands["golden"])
+
+    bronze >> silver >> golden
 
 dag_instance = dbt_dag()
