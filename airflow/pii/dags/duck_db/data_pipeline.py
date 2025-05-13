d8b458fa4101791f91ecaa947c2b8ae897796240
nazar.khimin <nazar.khimin@unanet.com>
2025-05-13 14:57:03 +0300

    Converted to single dag

--- airflow/pii/dags/duck_db/data_pipeline.py
+++ airflow/pii/dags/duck_db/data_pipeline.py
@@ -1,9 +1,11 @@
 import csv
 import logging
 import random
+import subprocess
 from datetime import date
 
-from airflow.sdk import task, dag
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import task, dag, task_group
 from faker import Faker
 from pendulum import datetime
 
@@ -50,16 +52,44 @@ def generate_raw_data_and_write_to_csv(data_csv: str):
             writer.writerow(_generate_raw_data(fake))
     logging.info(f"Written {rows} records to the CSV file {data_csv}")
 
+@task()
+def generate_data_task():
+    file_path = "/opt/airflow/data/raw_data.csv"
+    generate_raw_data_and_write_to_csv(file_path)
+    return file_path
+
 @dag(schedule=None,
      start_date=datetime(2025, 5, 7),
      catchup=False)
-def raw_generation_dag():
-    @task()
-    def generate_data_task():
-        file_path = "/opt/airflow/data/raw_data.csv"
-        generate_raw_data_and_write_to_csv(file_path)
-        return file_path
+def pipeline_duckdb_dag():
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
 
-    generate_data_task()
+    @task
+    def run_dbt(command: str):
+        subprocess.run(command, shell=True, check=True)
 
-dag_instance = raw_generation_dag()
+    @task_group(group_id="data_preparation")
+    def data_preparation():
+        generate_data_task()
+
+    @task_group(group_id="dbt_pipeline")
+    def dbt_pipeline():
+        dbt_run = "set -x; cd /opt/airflow/dbt && dbt run"
+        dbt_commands = {
+            "bronze": f"{dbt_run} --select tag:bronze --target prod",
+            "silver": f"{dbt_run} --select tag:silver --target prod",
+            "golden": f"{dbt_run} --select tag:golden --target prod"
+        }
+
+        bronze = run_dbt.override(task_id="run_bronze")(dbt_commands["bronze"])
+        silver = run_dbt.override(task_id="run_silver")(dbt_commands["silver"])
+        golden = run_dbt.override(task_id="run_golden")(dbt_commands["golden"])
+        bronze >> silver >> golden
+
+    prep = data_preparation()
+    dbt = dbt_pipeline()
+
+    start >> prep >> dbt >> end
+
+dag_instance = pipeline_duckdb_dag()
